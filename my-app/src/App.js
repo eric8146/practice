34ce90e0257b6a507dbd3e032cbd138c24a2d57d
eric8146 <97926348+eric8146@users.noreply.github.com>
2023-07-14 13:44:21 +0900

    aaa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link qweqweqwe"
+          className="App-link qe"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
